ca780b1f7f88e8bf3a97fec60b1e3160cbce84a8
VetalG <goolko.v@ukr.net>
2017-06-08 22:21:19 +0300

    mainPage v1

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
 window.onload=function(){
-	
+	$("#m_header-carousel").owlCarousel({
+  	items: 1,
+  	loop:true,
+  	dotsEach: true,
+  	autoplay: true,
+  	autoplayTimeout: 3000,
+  	autoplaySpeed: 1000,
+  	autoplayHoverPause:true
+  });
 };
