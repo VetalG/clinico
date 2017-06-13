3696385446075941be5af968035ee471cc3513d6
VetalG <goolko.v@ukr.net>
2017-06-13 22:59:40 +0300

    some fixes

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,32 @@
 window.onload=function(){
 	
 	$(".single_service, .Bread_crumbs .root").click(function(){
-		$("#slider_top, #services, #sercices_holder, .chose_hospital, .m_specialist_wrap, .Bread_crumbs .root + span, .Bread_crumbs .root + span + span,  .Bread_crumbs .root + span + span +span, .Bread_crumbs .root + span + span + span + span").fadeOut(500);
+		$("#slider_top, #services, #sercices_holder, .chose_hospital, .m_specialist_wrap").fadeOut(500);
 		$(".sinlge_servise_viwer").delay(500);
 		$(".sinlge_servise_viwer, .Bread_crumbs, .Bread_crumbs .root").fadeIn(500);
+        setTimeout(function(){ $('.service_info').equalHeights()}, 500);
 		return false;
 	});
 
-  $(".sinlge_servise_viwer aside ul li, .Bread_crumbs .root1").click(function(){
-		$("#slider_top, .sinlge_servise_viwer, .m_specialist_wrap, .Bread_crumbs .root1 + span, .Bread_crumbs .root2, #services, #sercices_holder, .Bread_crumbs .root + span, .Bread_crumbs .root + span + span").fadeOut(500);
+  $(".specific_service, .Bread_crumbs .root1").click(function(){
+		$("#slider_top, .sinlge_servise_viwer, .m_specialist_wrap, .Bread_crumbs .root2, #services, #sercices_holder").fadeOut(500);
 		$(".chose_hospital").delay(500);
-		$(".chose_hospital, .Bread_crumbs .root + span, .Bread_crumbs .root + span + span").fadeIn(500);
+		$(".chose_hospital, .root1").fadeIn(500);
         setTimeout(OneLineImg, 500, '.hospital img');
         return false;
 	});
 
   $(".single_hospital, .Bread_crumbs .root2").click(function(){
-        $("#slider_top, .sinlge_servise_viwer, #services, #sercices_holder, .chose_hospital, .Bread_crumbs .root + span, .Bread_crumbs .root + span + span, .Bread_crumbs .root2 + span, .Bread_crumbs .root3, .calendar_holder").fadeOut(500);
+        $("#slider_top, .sinlge_servise_viwer, #services, #sercices_holder, .chose_hospital, .Bread_crumbs .root3, .calendar_holder").fadeOut(500);
         $(".m_specialist_wrap").delay(500);
-        $(".m_specialist_wrap, .Bread_crumbs .root + span, .Bread_crumbs .root + span + span, .Bread_crumbs .root + span + span +span, .Bread_crumbs .root + span + span + span + span").fadeIn(500);
+        $(".m_specialist_wrap, .root2").fadeIn(500);
        return false;
   });
 
   $(".m_single_specialist, .i_dont_care button").click(function(){
         $(".m_specialist_wrap").fadeOut(500);
         $(".calendar_holder").delay(500);
-        $(".calendar_holder, .Bread_crumbs .root2 + span, .Bread_crumbs .root3").fadeIn(500);
+        $(".calendar_holder, .root3").fadeIn(500);
   });
 
    $('.moreDates').click(function(){
@@ -33,10 +34,9 @@ window.onload=function(){
         $(".date").click(function(){
         $(".m_specialist_wrap, .calendar_holder").fadeOut(500);
         $(".Make_an_Appointment").delay(500);
-        $(".Make_an_Appointment, .Bread_crumbs .root3 + span, .Bread_crumbs .root4").fadeIn(500);
+        $(".Make_an_Appointment, .Bread_crumbs .root4").fadeIn(500);
         });
    });
-
 
   function OneLineImg(stringSelectorImgColection){
     var colection=$(stringSelectorImgColection), MaxH=0;
@@ -50,7 +50,7 @@ window.onload=function(){
   }
 
   $('#cont').click(function(){
-    $("#slider_top, .sinlge_servise_viwer, #services, #sercices_holder, .chose_hospital, .m_specialist_wrap, .Bread_crumbs").fadeOut(500);
+    $("#slider_top, .sinlge_servise_viwer, #services, #sercices_holder, .chose_hospital, .m_specialist_wrap, .Make_an_Appointment, .Bread_crumbs").fadeOut(500);
     $('.contacts_page').delay(500);
     $('.contacts_page').fadeIn(500);
     return false;
@@ -76,6 +76,8 @@ window.onload=function(){
   	autoplayTimeout: 3000,
   	autoplaySpeed: speed,
   	navSpeed:speed,
+    nav: true,
+    navText: ['<img src="img/back.png" alt="prev">','<img src="img/next.png" alt="next">'],
   	autoplayHoverPause:true,
   	responsive: {
   		769:{
@@ -137,6 +139,33 @@ window.onload=function(){
    //time
    $('.appointment ul li.available').click(function(){
       $(this).toggleClass( "chosen" );
+   });
+
+   $('div.chosen').mouseover(function(){
+        $('li.chosen').css({'background-color':'Lime'});
+   }); 
+   $('div.chosen').mouseleave(function(){
+        $('li.chosen').removeAttr('style');
+   });
+
+   var div_not_available=$('div.not_available'),
+       li_not_available=$('li.not_available');
+   div_not_available.mouseover(function(){
+        li_not_available.css('color','red');
+   });
+   div_not_available.mouseleave(function(){
+       if(li_not_available[0].offsetHeight>0) li_not_available.removeAttr('style');
+   });
+   div_not_available.click(function(){
+        if(li_not_available[0].offsetHeight>0) li_not_available.slideUp(400);
+        else li_not_available.slideDown(400);
+   });
+
+   $('div.available').mouseover(function(){
+        $('li.available:not(.chosen)').css('background-color','lightBlue');
+   }); 
+   $('div.available').mouseleave(function(){
+        $('li.available').removeAttr('style');
    });
 
 };
